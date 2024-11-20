37301aa343fa7acec0407c59911e1167017003ce
Sivadasan Nair <NairSoorajS@johndeere.com>
2024-11-20 14:32:51 -0600

    chore: added dotenv for api keys

--- shipstation/utils/auth.js
+++ shipstation/utils/auth.js
@@ -1,7 +1,6 @@
-const API_KEY = "f11d313162d24362bcf8fc1e812f0033";
-const API_SECRET = "dc4f3766f6d54d3eb198aacd8a856742";
+require('dotenv').config();
 
-const getAuthToken = () => Buffer.from(`${API_KEY}:${API_SECRET}`).toString('base64');
+const getAuthToken = () => Buffer.from(`${process.env.SHIPSTATION_API_KEY}:${process.env.SHIPSTATION_API_SECRET}`).toString('base64');
 
 module.exports = {
     getAuthToken
