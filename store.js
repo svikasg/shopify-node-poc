75aeada3c75f4127479b7a9043d575fe4b602112
Sai Vikas Gopal <GopalSaiVikas@JohnDeere.com>
2024-11-21 13:01:03 -0500

    add port

--- store.js
+++ store.js
@@ -4,6 +4,7 @@ const getRawBody = require('raw-body');
 const crypto = require('crypto');
 const secretKey = '02234ea9833ddb55c050666b355e7471c5af9ba201a9c6667fb146f99ab307d0';
 const pool = require('./db');
+const port = process.env.PORT || 4000;
 
 app.post('/webhooks/cart/create', async (req, res) => {
     const hmac = req.get('X-Shopify-Hmac-Sha256');
@@ -32,4 +33,4 @@ app.post('/webhooks/cart/create', async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log('Shopify BE app listening on port 3000!'))
+app.listen(port, () => console.log('Shopify BE app listening on port 3000!'))
